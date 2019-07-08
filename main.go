5825a7eb21e128f4b02a6fc63d1509c6549199ee
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2019-07-08 17:35:31 +0200

    0.1.3 bool analyse

--- main.go
+++ main.go
@@ -8,16 +8,20 @@ import (
 )
 
 var ws bool
+var an bool
 
 func main() {
 	var file string
 
 	flag.StringVar(&file, "f", "", "file to analyse")
 	flag.BoolVar(&ws, "ws", false, "frequency with white spaces")
+	flag.BoolVar(&an, "a", true, "analyse file")
 	flag.Parse()
 	raw, err := ioutil.ReadFile(file)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Nelze přečíst soubor")
 	}
-	analyse(raw)
+	if an {
+		analyse(raw)
+	}
 }
